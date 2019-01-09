e86f712703236a7e9fc394e99c38feafe97c798e
Tassilo Posegga <tassilo.posegga@gmail.com>
2019-01-09 20:13:34 +0100

    Check if service App is installed

--- app/src/main/java/com/mmstechnology/serviceanalytics/MainActivity.kt
+++ app/src/main/java/com/mmstechnology/serviceanalytics/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.mmstechnology.serviceanalytics
 
-import android.support.v7.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            text.text = "Service App installed: ${isServiceAppInstalled(PACKAGE_NAME)}"
+        }
+    }
+
+    private fun isServiceAppInstalled(packageName: String): Boolean {
+        return try {
+            packageManager.getApplicationInfo(packageName, 0)
+            true
+        } catch (e: PackageManager.NameNotFoundException) {
+            false
+        }
+    }
+
+    companion object {
+        const val PACKAGE_NAME = "com.mmstechnology.service"
     }
 }
